27d2fce155c45ea64934a48c7f96ca7f4b8aba06
vert@vert.com <vert@vert.com>
2023-03-20 16:05:37 -0400

    clean

--- Marlin/Configuration.h
+++ Marlin/Configuration.h
@@ -628,14 +628,14 @@
 // Above this temperature the heater will be switched off.
 // This can protect components from overheating, but NOT from shorts and failures.
 // (Use MINTEMP for thermistor short/failure protection.)
-#define HEATER_0_MAXTEMP 315
-#define HEATER_1_MAXTEMP 315
-#define HEATER_2_MAXTEMP 315
-#define HEATER_3_MAXTEMP 315
-#define HEATER_4_MAXTEMP 315
-#define HEATER_5_MAXTEMP 315
-#define HEATER_6_MAXTEMP 315
-#define HEATER_7_MAXTEMP 315
+#define HEATER_0_MAXTEMP 295
+#define HEATER_1_MAXTEMP 295
+#define HEATER_2_MAXTEMP 295
+#define HEATER_3_MAXTEMP 295
+#define HEATER_4_MAXTEMP 295
+#define HEATER_5_MAXTEMP 295
+#define HEATER_6_MAXTEMP 295
+#define HEATER_7_MAXTEMP 295
 #define BED_MAXTEMP      125
 #define CHAMBER_MAXTEMP  60
 
@@ -645,7 +645,7 @@
  * (especially before PID tuning). Setting the target temperature too close to MAXTEMP guarantees
  * a MAXTEMP shutdown! Use these values to forbid temperatures being set too close to MAXTEMP.
  */
-#define HOTEND_OVERSHOOT 15   // (°C) Forbid temperatures over MAXTEMP - OVERSHOOT
+#define HOTEND_OVERSHOOT  5   // (°C) Forbid temperatures over MAXTEMP - OVERSHOOT
 #define BED_OVERSHOOT    10   // (°C) Forbid temperatures over MAXTEMP - OVERSHOOT
 #define COOLER_OVERSHOOT  2   // (°C) Forbid temperatures closer than OVERSHOOT
 
