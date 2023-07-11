046829e0c01a3483e836c8140670583b11d982a7
Vert <Vert@vert.com>
2023-07-11 12:59:02 -0400

    clean up

--- Marlin/Configuration.h
+++ Marlin/Configuration.h
@@ -2364,6 +2364,7 @@
 #define PREHEAT_1_TEMP_CHAMBER 35
 #define PREHEAT_1_FAN_SPEED     0 // Value from 0 to 255
 
+#if ENABLED (FULL_HIGH_TEMP)
 #define PREHEAT_2_LABEL       "ABS"
 #define PREHEAT_2_TEMP_HOTEND 250
 #define PREHEAT_2_TEMP_BED    110
@@ -2381,6 +2382,7 @@
 #define PREHEAT_4_TEMP_BED    110
 #define PREHEAT_4_TEMP_CHAMBER 35
 #define PREHEAT_4_FAN_SPEED     0 // Value from 0 to 255
+#endif
 
 // @section motion
 
